a630191d76d1d02f399fb4462c39d88f5600a7c4
egor600 <67320900+egor600@users.noreply.github.com>
2020-07-03 14:54:45 +0300

    Delete image.py

--- image.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://banner2.cleanpng.com/20180425/pke/kisspng-python-computer-icons-font-awesome-technology-circle-dots-floating-material-5ae020c2d20263.6237444615246378908602.jpg'
-
-r = requests.get(url)
-
-with open('image_py.jpg', 'wb') as file:
-	file.write(r.content)
